953855818ca937e058699d453d7e2a52dd03c012
Henrique <henrique.hfr2006@gmail.com>
2022-01-04 16:29:57 -0300

    main

--- src/component/ChatIntro/ChatIntro.js
+++ src/component/ChatIntro/ChatIntro.js
@@ -5,7 +5,7 @@ import './ChatIntro.css'
 function ChatIntro(){
     return(
         <div className='chatintro'>
-            <img src="https://web.whatsapp.com/img/intro-connection-light_c98cc75f2aa905314d74375a975d2cf2.jpg" alt="Olá mundo"/>
+            <img src='./whatsappInicio.jpg' alt="Olá mundo"/>
             <h1>Mantenha seu celular conectado</h1>
             <h2>O WhatsApp conecta o seu telefone para sincronizar suas mensagens.<br/>Para reduzir o uso de daos, conecte seu telefone a uma rede Wi-FI.</h2>
         </div>
