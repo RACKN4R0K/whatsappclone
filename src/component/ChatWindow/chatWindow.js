b2c6c2f54fb1f5eea827e58ccda7b5bf66cc7820
Henrique <henrique.hfr2006@gmail.com>
2022-01-08 20:58:31 -0300

    main

--- src/component/ChatWindow/chatWindow.js
+++ src/component/ChatWindow/chatWindow.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import './chatWindow.css'
+import EmojiPicker from 'emoji-picker-react';
 
 function chatWindow() {
+    const handleEmokiClick = ()=>{
+
+    }
     return (
         <div className='chatWindow'>
             <div className='chatWindow-header'>
@@ -48,24 +52,40 @@ function chatWindow() {
             <div className='chatWindow-body'>
 
             </div>
+
+            <div className='chatWindow-emoji-area'>
+                <EmojiPicker 
+                onEmojiClick={handleEmokiClick}
+                disableSearchBar
+                disableSkinTonePicker
+                />
+            </div>
             <div className='chatWindow-footer'>
                 <div className='chatWindow-pre'>
                     <div className='chatWindow-buttons'>
                         {/*botao de emoji*/}
                         <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><g><rect fill="none" height="24" width="24" /></g><g><g /><g><circle cx="15.5" cy="9.5" r="1.5" /><circle cx="8.5" cy="9.5" r="1.5" /><path d="M12,18c2.28,0,4.22-1.66,5-4H7C7.78,16.34,9.72,18,12,18z" /><path d="M11.99,2C6.47,2,2,6.48,2,12c0,5.52,4.47,10,9.99,10C17.52,22,22,17.52,22,12C22,6.48,17.52,2,11.99,2z M12,20 c-4.42,0-8-3.58-8-8c0-4.42,3.58-8,8-8s8,3.58,8,8C20,16.42,16.42,20,12,20z" /></g></g></svg>
-                        {/*{/*botao de fechar emoji 
-                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none" /><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z" /></svg>*/}
                     </div>
-                    <div className='chatWindow-input-area'>
+                    {/*botao de fechar emoji */}
+                    <div className='chatWindow-buttons'>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none" /><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z" /></svg>
+                    </div>
 
-                    </div>
-                    <div className='chatWindow-pois'>
-                        <div className='chatWindow-buttons'>
-                            {/*botao de envio de mensagem */}
-                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none" /><path d="M4.01 6.03l7.51 3.22-7.52-1 .01-2.22m7.5 8.72L4 17.97v-2.22l7.51-1M2.01 3L2 10l15 2-15 2 .01 7L23 12 2.01 3z" /></svg>
-                            {/*{/*botao de microfone 
+
+                </div>
+                <div className='chatWindow-input-area'>
+                    <input className='chatWindow-input'
+                        type="text"
+                        placeholder='Digite uma mensagem...'
+
+                    ></input>
+                </div>
+                <div className='chatWindow-pois'>
+                    <div className='chatWindow-buttons'>
+                        {/*botao de envio de mensagem */}
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none" /><path d="M4.01 6.03l7.51 3.22-7.52-1 .01-2.22m7.5 8.72L4 17.97v-2.22l7.51-1M2.01 3L2 10l15 2-15 2 .01 7L23 12 2.01 3z" /></svg>
+                        {/*{/*botao de microfone 
                         <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><g><rect fill="none" height="24" width="24" /><rect fill="none" height="24" width="24" /><rect fill="none" height="24" width="24" /></g><g><g /><g><path d="M12,14c1.66,0,3-1.34,3-3V5c0-1.66-1.34-3-3-3S9,3.34,9,5v6C9,12.66,10.34,14,12,14z" /><path d="M17,11c0,2.76-2.24,5-5,5s-5-2.24-5-5H5c0,3.53,2.61,6.43,6,6.92V21h2v-3.08c3.39-0.49,6-3.39,6-6.92H17z" /></g></g></svg>*/}
-                        </div>
                     </div>
                 </div>
             </div>
